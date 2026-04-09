eb50ffb18a5e96b11549d4a57aa0e0b9cc66c451
Kevan <kevan@yourdomain.com>
2026-04-09 12:08:02 -0400

    chore: gitignore build output (apps/web/out)

--- apps/web/out/_next/static/QUx9WAPvndvIQFS_USG62/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fcourses\u002F[trackSlug]","\u002Fdashboard\u002Fcourses\u002F[trackSlug]\u002F[lessonSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
